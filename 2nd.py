875e7659045780215fa2f65f905f09319a761bb9
RISA-source <rishisaraffofficial@gmail.com>
2024-08-10 21:45:41 +0545

    Basic Arithmetic

---
+++ 2nd.py
@@ -0,0 +1,13 @@
+# Basic Arithmetic: Create a program that performs basic arithmetic operations (addition, subtraction, multiplication, division).
+
+x = 10
+y = 5
+a = x + y
+s = x - y
+m = x * y
+d = x / y
+
+print ('Addition:', a)
+print ('Substraction:', s)
+print ('Multiplication:', m)
+print ('Division:', d)
