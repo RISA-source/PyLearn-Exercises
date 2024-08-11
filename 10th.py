d8863d0a43f162cd7beb9dcbb4892fb7504814e8
RISA-source <rishisaraffofficial@gmail.com>
2024-08-11 12:08:36 +0545

    Basic List Operations

---
+++ 10th.py
@@ -0,0 +1,12 @@
+# Basic List Operations: Perform operations like append, remove, sort, and reverse on a list.
+
+l = [1,2,3,4,5]
+print(l)
+l.append(6)
+print(l)
+l.remove(6)
+print(l)
+l.reverse()
+print(l)
+l.sort()
+print(l)
