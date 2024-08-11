671b7bac5548fbd1008942dcc20c8e413348143c
RISA-source <rishisaraffofficial@gmail.com>
2024-08-11 15:57:06 +0545

    File Handling

---
+++ 11th.py
@@ -0,0 +1,11 @@
+# File Handling: Create a program to read from and write to a text file.
+
+file = open ('readWrite.txt','w')
+file.write('This file is automatically created when running the 11th python program.\n')
+file.write('This is a subordinate file created by this program for fulfilment of the given task.')
+file.close()
+
+file1 = open ('readWrite.txt')
+c = file1.read()
+print(c)
+file1.close()
