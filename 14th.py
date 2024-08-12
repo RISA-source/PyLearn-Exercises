625e9427d197b67449b0f3c06863eef95752128a
RISA-source <rishisaraffofficial@gmail.com>
2024-08-12 09:56:20 +0545

    Dictionary Operations

---
+++ 14th.py
@@ -0,0 +1,14 @@
+# Dictionary Operations: Perform operations like adding, updating, and deleting key-value pairs in a dictionary.
+
+d = dict()
+d['a'] = 10
+d['b'] = 32
+d['c'] = 45
+d['d'] = 65     #Adding to the dictionary
+print(d)
+
+d['c'] = 86     #Appending the existing Dictionary
+print(d)
+
+del d['b']      #Deleting key-value pair from the dictionary
+print (d)
